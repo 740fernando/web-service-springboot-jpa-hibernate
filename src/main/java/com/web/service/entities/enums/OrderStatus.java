3634860958cf8373ea623df531b045f81993cd97
fsouviei <fernando.souzavieira@everis.nttdata.com>
2022-02-09 13:59:40 -0300

    Enum OrderStatus

---
+++ src/main/java/com/web/service/entities/enums/OrderStatus.java
@@ -0,0 +1,34 @@
+package com.web.service.entities.enums;
+
+public enum OrderStatus {
+
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERD(4),
+	CANCELED(5);
+	
+	// codigo para o tipo enum
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	//acessar o code
+	public int getCode() {
+		return code;
+	}
+	
+	// Método estático porque vai funcionar sem precisar instanciar
+	// percorre os valores do OrderStatus e  compara se o 'code' passado no argumento é igual ao value.getcode()
+	// caso seja igual é retornado, caso não é lançado uma excessão
+	public static OrderStatus valueOf(int code) {
+		for(OrderStatus value : OrderStatus.values()) {
+			if(value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Código inválido");
+	}
+}
