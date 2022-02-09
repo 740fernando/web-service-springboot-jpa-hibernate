4c90d014fc045bb937283b0dd7704c322637711f
fsouviei <fernando.souzavieira@everis.nttdata.com>
2022-02-09 08:51:46 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/web/service/repositories/UserRepository.java
+++ src/main/java/com/web/service/repositories/UserRepository.java
@@ -1,11 +1,15 @@
 package com.web.service.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.web.service.entities.User;
-
+/**
+ * 
+ * A classe UserRepository não é necessário de annotation(Repository) pois está herdando da interface JpaRepository, que já possui a annotation implementada.
+ * 
+ * @author fsouviei
+ *
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 
