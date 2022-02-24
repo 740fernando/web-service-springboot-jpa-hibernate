bc51ced697182f9345af1f2937b4df25fb0c5d61
fsouviei <fernando.souzavieira@everis.nttdata.com>
2022-02-24 18:12:26 -0300

    Entidade Payment, associação um para um

--- src/main/java/com/web/service/config/TesteConfig.java
+++ src/main/java/com/web/service/config/TesteConfig.java
@@ -16,6 +16,7 @@ import com.web.service.model.dao.repositories.UserRepository;
 import com.web.service.model.entities.Category;
 import com.web.service.model.entities.Order;
 import com.web.service.model.entities.OrderItem;
+import com.web.service.model.entities.Payment;
 import com.web.service.model.entities.Product;
 import com.web.service.model.entities.User;
 import com.web.service.model.entities.enums.OrderStatus;
@@ -96,5 +97,11 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3,p5,2,p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1); //salvar o pedido, o jpa vai se encarregar de salvar o Payment
 	}
 }
