3693c9255561f487513d612a4824a0bac704fd81
fsouviei <fernando.souzavieira@everis.nttdata.com>
2022-02-25 11:31:57 -0300

    Inserção de User

--- src/main/java/com/web/service/controller/resources/UserResource.java
+++ src/main/java/com/web/service/controller/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.web.service.controller.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.web.service.model.entities.User;
 import com.web.service.model.services.UserService;
@@ -40,5 +44,13 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping // Faz um pré processamento na compilação do compilador, definindo que este método vai receber um o método post do http
+	public ResponseEntity<User> insert(@RequestBody User obj){ // @RequestBody -> Para dizer para método que esse objeto vai chegar em formato json e seria deserializado para um objeto User usamos a anotação RequestBody
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
