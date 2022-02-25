e5bceaf575f9204a91c65d6c70e2e329a7cbf8b5
fsouviei <fernando.souzavieira@everis.nttdata.com>
2022-02-25 13:59:24 -0300

    Tratamento de exceção - findById

---
+++ src/main/java/com/web/service/controller/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.web.service.controller.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.web.service.model.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice // Intercepta as excecoes que acontecerem p/ q objeto possa executar um possivel tratamento
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)//Com essa anotação, eu estou falando para o sistema, interceptar qualquer excecão do tipo ResourceNotFoundException
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Recurso não encontrado";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(),error,e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+	}
+}
--- src/main/java/com/web/service/model/services/UserService.java
+++ src/main/java/com/web/service/model/services/UserService.java
@@ -3,41 +3,41 @@ package com.web.service.model.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.ResourceClosedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.web.service.model.dao.repositories.UserRepository;
 import com.web.service.model.entities.User;
+import com.web.service.model.services.exceptions.ResourceNotFoundException;
 
 /**
  * 
  * Component - Indica que uma classe anotada é um "componente". Essas classes
- *            são consideradas candidatas à detecção automática ao usar
- *            configuração baseada em anotação e varredura de caminho de classe.
+ * são consideradas candidatas à detecção automática ao usar configuração
+ * baseada em anotação e varredura de caminho de classe.
  * 
- * Service- Indica que uma classe anotada é um "Serviço", originalmente
- *           definido pelo Domain-DrivenDesign (Evans, 2003) como "uma operação
- *           oferecida como uma interface que fica sozinha no modelo, sem estado
- *           encapsulado".Também pode indicar que uma classe é uma "Fachada de
- *           Serviço de Negócios" (no sentido Core J2EEpatterns), ou algo
- *           semelhante. Essa anotação é um estereótipo de uso geral e equipes
- *           individuais podem restringir sua semântica e usar conforme
- *           apropriado.
+ * Service- Indica que uma classe anotada é um "Serviço", originalmente definido
+ * pelo Domain-DrivenDesign (Evans, 2003) como "uma operação oferecida como uma
+ * interface que fica sozinha no modelo, sem estado encapsulado".Também pode
+ * indicar que uma classe é uma "Fachada de Serviço de Negócios" (no sentido
+ * Core J2EEpatterns), ou algo semelhante. Essa anotação é um estereótipo de uso
+ * geral e equipes individuais podem restringir sua semântica e usar conforme
+ * apropriado.
  * 
- * Optional- Um objeto container que pode ou não conter um valor não
- *           nulo. Se um valor estiver presente, isPresent() retornará true. Se
- *           nenhum valor estiver presente, o objeto será considerado vazio e
- *           isPresent() retornará false.
+ * Optional- Um objeto container que pode ou não conter um valor não nulo. Se um
+ * valor estiver presente, isPresent() retornará true. Se nenhum valor estiver
+ * presente, o objeto será considerado vazio e isPresent() retornará false.
  * 
- *           Métodos adicionais que dependem da presença ou ausência de um valor
- *           contido são fornecidos, como orElse() (retorna um valor padrão se
- *           nenhum valor estiver presente) e ifPresent() (executa uma ação se
- *           um valor estiver presente).
+ * Métodos adicionais que dependem da presença ou ausência de um valor contido
+ * são fornecidos, como orElse() (retorna um valor padrão se nenhum valor
+ * estiver presente) e ifPresent() (executa uma ação se um valor estiver
+ * presente).
  * 
- *           Esta é uma classe baseada em valor; o uso de operações sensíveis à
- *           identidade (incluindo igualdade de referência(==), código de hash
- *           de identidade ou sincronização) em instâncias de Optional pode ter
- *           resultados imprevisíveis e deve ser evitado.
+ * Esta é uma classe baseada em valor; o uso de operações sensíveis à identidade
+ * (incluindo igualdade de referência(==), código de hash de identidade ou
+ * sincronização) em instâncias de Optional pode ter resultados imprevisíveis e
+ * deve ser evitado.
  * 
  * 
  * @author fsouviei
@@ -56,19 +56,20 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = repository.getById(id); // getById - vai deixar o obojeto monitorado pelo jpa, para em seguida fazer uma operação com banco de dados
+		User entity = repository.getById(id); // getById - vai deixar o obojeto monitorado pelo jpa, para em seguida
+												// fazer uma operação com banco de dados
 		updateData(entity, obj);
 		return repository.save(entity);
 	}
@@ -76,6 +77,6 @@ public class UserService {
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getPhone());		
+		entity.setPhone(obj.getPhone());
 	}
 }
